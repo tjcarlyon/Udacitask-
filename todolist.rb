95f78a3a4bdde3fa224cf41976252ed79af598d3
tjcarlyon <tjcarlyon@gmail.com>
2016-03-19 13:24:52 -0500

    inital commit

---
+++ todolist.rb
@@ -0,0 +1,11 @@
+class TodoList
+     # Initialize todo list with a title and no items
+     def initialize(list_title)
+        @title = list_title
+        @items = Array.new # Starts empty! No Items yet!
+     end
+end
+
+class Item
+    # methods and stuff go here
+end
