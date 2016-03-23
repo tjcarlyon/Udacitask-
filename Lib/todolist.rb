960230cfc8c48858744990d74263afb7af86a741
tjcarlyon <tjcarlyon@gmail.com>
2016-03-22 19:22:36 -0500

    working on methods

--- Lib/todolist.rb
+++ Lib/todolist.rb
@@ -1,85 +1,104 @@
-#class User
-    #attr_reader :username
-    #def initialize(username)
-        #@username = username
-    #end
-#end
-
-
-
 class TodoList
-    #add user to attr_reader
+  # methods and stuff go here
   attr_reader :title, :items
 
-    # creating a list w title
-    #add user to argument)
-    def initialize(list_title)
-       @title = list_title
-       @items = Array.new # Starts empty! No Items yet!
-       #@user = user
-    end
+  ## Initialize todo list with a title and no items
+  def initialize(list_title)
+    @title = list_title
+    @items = Array.new
+  end
 
-    # renaming list title
-    def rename_title(new_title)
-      @title = new_title
-    end
+  ## Rename the existing title
+   def rename_title(new_title)
+    @title = new_title
+  end
 
-    # adding items
-    def add_item(new_item)
-       item = Item.new(new_item)
-       @items.push(item)
-    end
+  ## Add an Item to the array of Items
+  def add_item(new_item)
+    item = Item.new new_item
+    @items.push item
+  end
 
-    # deleting items
-    def remove_item(index)
-      @items.delete_at(index)
-    end
+  ## Remove a taks from list
+  def remove_item(item_id)
+    @items.delete_at item_id-1
+  end
 
-    # print
-    def print_list
-        puts "*" * 25
-        #add user?
-        puts "#{title}"
-        puts "*" * 25
-        #counter to add number before each item
-        i = 1
-        @items.each do |item|
-        puts "#{i}" + "  " + item.description + "   Completed: " + item.completed?
-        i += 1
+  ## Flag the task as completed
+  def completed_item(item_id)
+    if indexed? item_id
+      @items[item_id-1].completed_item
     end
-      
+  end
 
+  ## Remove completed flag from the task
+  def uncompleted_item(item_id)
+    if indexed? item_id
+      @items[item_id-1].uncompleted_item
     end
+  end
+
+# Add due date to task using yy-mm-dd format
+  #def add_due_date(item_id, due_date)
+    #if indexed? item_id
+      #@items[item_id-1].set_due_date due_date
+    #end
+  #end
+
+  ## Remove due date from task
+  def remove_due_date(item_id)
+    if indexed? item_id
+      @items[item_id-1].remove_date
+    end
+  end
+
+  
+
+  # Return the index of the task
+  def get_item_index(item)
+    @items.index item
+  end
+
+  ## Print the title and call the Item print method
+  def print_list
+    puts "*" * 25
+    puts "\n"
+    puts "#{@title}"
+    puts "*" * 25
+    @items.each do |item|
+      item_index = get_item_index item
+      item.print_item(item_index + 1)
+    end
+  end
 end
-
 
 
 
 class Item
-    # methods and stuff go here
+  
+  # methods  go here
+  attr_accessor :description, :completed_status, #:due_date  
 
-     attr_reader :description, :completed_status
-
-     # Initialize item with a description and marked as
-     # not complete
-     def initialize(item_description)
-        @description = item_description
-        @completed_status = false
-        
-     end
-
-
-    #returns a boolean
-     def completed? 
-        #Ternary operator expressions
-        return @completed_status? "yes" : "no"
-     end
-     
-
-     def toggle_status
-         @completed_status = @completed_status == true ? false : true
-     end
+  # Initailize item with a description, and not completed 
+  def initialize(item_description)
+    @description = item_description
+    @completed_status = false
     
+  end
 
-end
-   
+  # Check to see if item is marked as completed
+  def item_completed?
+    @completed_status
+  end
+
+  # mark item as complete
+  def completed_item
+    @completed_status = true
+  end
+
+  # Remove complete check for item
+  def uncompleted_item
+    @completed_status = false
+  end
+
+  
--- Lib/udacitask.rb
+++ Lib/udacitask.rb
@@ -2,38 +2,36 @@ require_relative 'todolist.rb'
 
 # Creates a new todo list
 list = TodoList.new("Pre-race Check List")
-#list = TodoList.new(User.new("Jeff"),"Pre-Race Check List")
 
 # Add four new items
 list.add_item("Sneakers")
-list.add_item("Bib")
+list.add_item("bib")
+list.add_item("Drink")
 list.add_item("Nutrition")
-list.add_item("Drink") 
 
 # Print the list
-list.print_list
+#list.print_list
 
 # Delete the first item
-list.remove_item(0)
-# Print the list
-list.print_list
-# Delete the second item
-list.remove_item(1)
+#list.remove_item(1)
 
 # Print the list
-list.print_list
+#list.print_list
+
+# Delete the second item
+#list.remove_item(2)
+
+# Print the list
+#list.print_list
 
 # Update the completion status of the first item to complete
-list.items[0].toggle_status
+#list.completed_item(1)
 
 # Print the list
-list.print_list
+#list.print_list
 
 # Update the title of the list
-list.rename_title("Do or die")
+#list.rename_title("Post Race")
 
 # Print the list
-list.print_list
-
-#create new user list
-#sherpa_list = TodoList.new(User.new("Thomas"), "Do or die")
+#list.print_list
