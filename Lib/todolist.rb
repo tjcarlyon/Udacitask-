bf328cec89e65c4a0cb555af24ef13b224b04d81
tjcarlyon <tjcarlyon@gmail.com>
2016-03-23 20:51:29 -0500

    updating methods

--- Lib/todolist.rb
+++ Lib/todolist.rb
@@ -1,69 +1,76 @@
-class TodoList
+ class TodoList
   # methods and stuff go here
   attr_reader :title, :items
 
   ## Initialize todo list with a title and no items
-  def initialize(list_title)
+  def initialize(list_title, user_name, time_created = Time.now.asctime)
     @title = list_title
     @items = Array.new
+    @user_name = user_name
+    @time_created = time_created
   end
 
-  ## Rename the existing title
+  # Rename the existing title
    def rename_title(new_title)
     @title = new_title
   end
 
-  ## Add an Item to the array of Items
+# Add an Item to the array of Items
   def add_item(new_item)
     item = Item.new new_item
     @items.push item
   end
 
-  ## Remove a taks from list
+
+  #name a new item and item num
+  def rename_item(item, description)
+    @items[item].description = description
+  end
+
+
+# Remove a taks from list
   def remove_item(item_id)
     @items.delete_at item_id-1
   end
 
-  ## Flag the task as completed
+  #mark the task as completed
   def completed_item(item_id)
     if indexed? item_id
       @items[item_id-1].completed_item
     end
   end
 
-  ## Remove completed flag from the task
+  # Remove completed flag from the task
   def uncompleted_item(item_id)
     if indexed? item_id
       @items[item_id-1].uncompleted_item
     end
   end
 
-# Add due date to task using yy-mm-dd format
-  #def add_due_date(item_id, due_date)
-    #if indexed? item_id
-      #@items[item_id-1].set_due_date due_date
-    #end
-  #end
 
-  ## Remove due date from task
-  def remove_due_date(item_id)
-    if indexed? item_id
-      @items[item_id-1].remove_date
+
+  # Check to see if task id provided by user is in the range of the items array
+  def indexed?(item_id)
+    size_of_index = @items.length
+    if item_id > size_of_index || item_id < 1
+      return false
+    else
+      return true
     end
   end
-
-  
 
   # Return the index of the task
   def get_item_index(item)
     @items.index item
   end
 
-  ## Print the title and call the Item print method
+  
+  # Print the title and call the Item print method
   def print_list
     puts "*" * 25
     puts "\n"
     puts "#{@title}"
+    puts "#{@time_created}"
     puts "*" * 25
     @items.each do |item|
       item_index = get_item_index item
@@ -76,15 +83,16 @@ end
 
 class Item
   
-  # methods  go here
-  attr_accessor :description, :completed_status, #:due_date  
+  # methods  go here  
+  attr_accessor :description, :completed_status
 
-  # Initailize item with a description, and not completed 
+  # Initailize item with a description and not completed 
   def initialize(item_description)
     @description = item_description
     @completed_status = false
     
-  end
+    
+   end
 
   # Check to see if item is marked as completed
   def item_completed?
@@ -96,9 +104,20 @@ class Item
     @completed_status = true
   end
 
-  # Remove complete check for item
+  # Remove complete mark for item
   def uncompleted_item
     @completed_status = false
   end
 
-  
+
+
+  # Print method for item, using cboxes to show complete
+  def print_item(index)
+    check_box = item_completed? ? "[x]" : "[ ]"
+    output_string = "#{check_box} #{index} - #{@description}"
+    
+    puts output_string
+  end
+
+ end
+
--- Lib/udacitask.rb
+++ Lib/udacitask.rb
@@ -1,7 +1,12 @@
 require_relative 'todolist.rb'
 
+puts "Your username:"
+user_name = gets.chomp
+puts "Hello #{user_name}, "
+
+
 # Creates a new todo list
-list = TodoList.new("Pre-race Check List")
+list = TodoList.new("Pre-race Check List", user_name)
 
 # Add four new items
 list.add_item("Sneakers")
@@ -9,29 +14,51 @@ list.add_item("bib")
 list.add_item("Drink")
 list.add_item("Nutrition")
 
-# Print the list
-#list.print_list
+#Print the list
+list.print_list
 
-# Delete the first item
-#list.remove_item(1)
+#Delete the first item
+list.remove_item(1)
 
-# Print the list
-#list.print_list
+#Print the list
+list.print_list
 
-# Delete the second item
-#list.remove_item(2)
+#Delete the second item
+list.remove_item(2)
 
-# Print the list
-#list.print_list
+#Print the list
+list.print_list
 
-# Update the completion status of the first item to complete
-#list.completed_item(1)
+#Update the completion status of the first item to complete
+list.completed_item(1)
 
-# Print the list
-#list.print_list
 
-# Update the title of the list
-#list.rename_title("Post Race")
+#Print the list
+list.print_list
 
-# Print the list
-#list.print_list
+ #Update the title of the list
+list.rename_title("Post Race")
+
+#Print the list
+list.print_list
+
+
+list.rename_item(0, "towel".upcase)
+list.print_list
+
+list.rename_item(1, "beer".upcase)
+list.print_list
+
+list.completed_item(2)
+list.print_list
+
+
+
+
+
+
+
+
+
+
+
