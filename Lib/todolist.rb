e0f4700b512bb766232e68a78c287e88abbfcf50
tjcarlyon <tjcarlyon@gmail.com>
2016-03-24 20:11:57 -0500

    cleaning up code

--- Lib/todolist.rb
+++ Lib/todolist.rb
@@ -2,7 +2,7 @@
   # methods and stuff go here
   attr_reader :title, :items
 
-  ## Initialize todo list with a title and no items
+  # Initialize todo list with a title and no items
   def initialize(list_title, user_name, time_created = Time.now.asctime)
     @title = list_title
     @items = Array.new
@@ -21,8 +21,7 @@
     @items.push item
   end
 
-
-  #name a new item and item num
+# name a new item and item num
   def rename_item(item, description)
     @items[item].description = description
   end
@@ -33,23 +32,21 @@
     @items.delete_at item_id-1
   end
 
-  #mark the task as completed
+# mark the task as completed
   def completed_item(item_id)
     if indexed? item_id
       @items[item_id-1].completed_item
     end
   end
 
-  # Remove completed flag from the task
+# Remove completed flag from the task
   def uncompleted_item(item_id)
     if indexed? item_id
       @items[item_id-1].uncompleted_item
     end
   end
 
-
-
-  # Check to see if task id provided by user is in the range of the items array
+# Check to see if task id provided by user is in the range of the items array
   def indexed?(item_id)
     size_of_index = @items.length
     if item_id > size_of_index || item_id < 1
@@ -59,13 +56,12 @@
     end
   end
 
-  # Return the index of the task
+# Return the index of the task
   def get_item_index(item)
     @items.index item
   end
 
-  
-  # Print the title and call the Item print method
+# Print the title and call the Item print method
   def print_list
     puts "*" * 25
     puts "\n"
@@ -80,13 +76,12 @@
 end
 
 
-
 class Item
   
-  # methods  go here  
+# methods  go here  
   attr_accessor :description, :completed_status
 
-  # Initailize item with a description and not completed 
+# Initailize item with a description and not completed 
   def initialize(item_description)
     @description = item_description
     @completed_status = false
@@ -94,26 +89,24 @@ class Item
     
    end
 
-  # Check to see if item is marked as completed
+# Check to see if item is marked as completed
   def item_completed?
     @completed_status
   end
 
-  # mark item as complete
+# mark item as complete
   def completed_item
     @completed_status = true
   end
 
-  # Remove complete mark for item
+# Remove complete mark for item
   def uncompleted_item
     @completed_status = false
   end
 
-
-
-  # Print method for item, using cboxes to show complete
+# Print method for item, using cboxes to show complete
   def print_item(index)
-    check_box = item_completed? ? "[x]" : "[ ]"
+    check_box = item_completed? ? "(x)" : "( )"
     output_string = "#{check_box} #{index} - #{@description}"
     
     puts output_string
